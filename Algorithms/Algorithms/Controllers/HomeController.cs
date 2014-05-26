a4f5b1047aed555a2cf5dcb44e6565250341bc88
onexsp <onesimo182@gmail.com>
2014-05-26 18:10:11 -0400

    Algoritms

---
+++ Algorithms/Algorithms/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Algorithms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Algorithms.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Palindrome()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Palindrome(AlgorithmsModel am)
+        {
+            
+            if (am.IsPalindrome(am.Text))
+                ViewBag.Indicator = "<div class='alert alert-success'>The string <strong>"+am.Text+"</strong> is Palindrome</div>";
+            else
+                ViewBag.Indicator = "<div class='alert alert-danger'>The string <strong>" + am.Text + "</strong> is not Palindrome</div>";
+            return View();
+        }
+
+        public ActionResult Fibonacci()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Fibonacci(AlgorithmsModel am)
+        {
+            ViewBag.Serie = "<div class='alert alert-success'>" + am.Fibonacci(am.Number) + "</div>";
+            return View();
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Models/AlgorithmsModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Algorithms.Models
+{
+    public class AlgorithmsModel
+    {
+        [Required(ErrorMessage = "You must enter a value to verify")]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = "You must enter a value to verify")]
+        public int Number { get; set; }
+
+        public bool IsPalindrome(string texto)
+        {
+            char[] original = new char[texto.Length];
+            string right = string.Empty;
+            string left = string.Empty;
+            bool isPalindrome = false;
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                original[i] = texto[i];
+                right += original[i];
+
+            }
+            Array.Reverse(original);
+
+            foreach (var item in original)
+            {
+                left += item;
+            }
+
+            if (left.Equals(right))
+                isPalindrome = true;
+            else
+                isPalindrome = false;
+
+            return isPalindrome;
+
+        }
+
+        public string Fibonacci(int number)
+        {
+            var first = 0;
+            var second = 1;
+            var result = 0;
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder ();
+            sb.Append(first+","+second+",");
+            while(result < number)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+                sb.Append(result + ",");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
